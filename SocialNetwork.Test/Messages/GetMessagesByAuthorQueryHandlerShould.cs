73835b0e1b2558661e8df33e74f4d22d30e65565
cvigueras <carlosj.vigueras@gmail.com>
2023-04-27 09:41:37 +0200

    Refactor: adapted test GetEmptyWhenGetOwnMessages to use MediaTR.

--- SocialNetwork.Test/Messages/GetMessagesByAuthorQueryHandlerShould.cs
+++ SocialNetwork.Test/Messages/GetMessagesByAuthorQueryHandlerShould.cs
@@ -1,20 +1,23 @@
 ﻿using FluentAssertions;
 using NSubstitute;
 using SocialNetwork.Api.Messages;
+using SocialNetwork.Api.Messages.Queries;
 using SocialNetwork.Api.Time;
 
 namespace SocialNetwork.Test.Messages
 {
-    public class TimeLineControllerShould
+    public class GetMessagesByAuthorQueryHandlerShould
     {
         private MessagesController _timelineController;
         private IMessagesRepository _messagesRepository;
+        private GetMessagesByAuthorQueryHandler _getMessagesByAuthorQueryHandlerShould;
         private ITime _time;
 
         [SetUp]
         public void SetUp()
         {
             _messagesRepository = Substitute.For<IMessagesRepository>();
+            _getMessagesByAuthorQueryHandlerShould = new GetMessagesByAuthorQueryHandler(_messagesRepository);
             _time = Substitute.For<ITime>();
             _timelineController = new MessagesController(_messagesRepository, _time);
         }
@@ -22,9 +25,10 @@ namespace SocialNetwork.Test.Messages
         [Test]
         public async Task GetEmptyWhenGetOwnMessages()
         {
+            var getMessagesByAuthorQuery = new GetMessagesByAuthorQuery("Alice");
             _messagesRepository.GetByAuthor("Alice").Returns(Enumerable.Empty<Message>());
 
-            var result = await _timelineController.Get("Alice");
+            var result = await _getMessagesByAuthorQueryHandlerShould.Handle(getMessagesByAuthorQuery, default);
 
             result.Should().BeEmpty();
         }
