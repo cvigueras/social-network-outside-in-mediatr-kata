982d8cb1db8ec18b199c9439ab3aa6ab7b8f59b9
cvigueras <carlosj.vigueras@gmail.com>
2023-05-02 12:35:03 +0200

    Refactor: removed comments.

--- SocialNetwork.Api/Program.cs
+++ SocialNetwork.Api/Program.cs
@@ -2,9 +2,9 @@ using SocialNetwork.Api;
 
 var builder = WebApplication.CreateBuilder(args);
 var startup = new Startup(builder.Configuration);
-startup.ConfigureServices(builder.Services); // calling ConfigureServices method
+startup.ConfigureServices(builder.Services);
 var app = builder.Build();
-startup.Configure(app, builder.Environment); // calling Configure method
+startup.Configure(app, builder.Environment);
 
 namespace SocialNetwork.Api
 {
