9609fd60f9a15a2ca2b5ff3926a271d384ef2920
cvigueras <carlosj.vigueras@gmail.com>
2023-05-02 12:29:08 +0200

    Refactor: chop long line.

--- SocialNetwork.Api/Messages/MessageRepository.cs
+++ SocialNetwork.Api/Messages/MessageRepository.cs
@@ -14,19 +14,25 @@ namespace SocialNetwork.Api.Messages
 
         public async Task Add(Message message)
         {
-            await _connection.ExecuteAsync($"INSERT INTO Messages(Author, Post, Timestamp) VALUES('{message.Author}', '{message.Post}', '{message.Timestamp:O}')");
+            await _connection.ExecuteAsync($"INSERT INTO Messages(Author, Post, Timestamp) " +
+                                           $"VALUES('{message.Author}', '{message.Post}', '{message.Timestamp:O}')");
         }
 
         public Task<IEnumerable<Message>> GetByAuthor(string? author)
         {
-            return _connection.QueryAsync<Message>($"SELECT Author, Post, Timestamp FROM Messages WHERE Author = '{author}' ORDER BY Timestamp DESC");
+            return _connection.QueryAsync<Message>($"SELECT Author, Post, Timestamp " +
+                                                   $"FROM Messages WHERE Author = '{author}' " +
+                                                   "ORDER BY Timestamp DESC");
         }
 
         public async Task<IEnumerable<Message>> GetByAuthorAndSubscriptions(string user)
         {
-            var messagesSubscription = await _connection.QueryAsync<Message>($"SELECT M.Author, M.Post, M.Timestamp FROM Messages M JOIN Subscriptions S ON M.Author = S.User WHERE S.Subscriber = '{user}'");
+            var messagesSubscription = await _connection.QueryAsync<Message>($"SELECT M.Author, M.Post, M.Timestamp " +
+                                                                             "FROM Messages M JOIN Subscriptions S ON M.Author = S.User " +
+                                                                             $"WHERE S.Subscriber = '{user}'");
 
-            var ownMessages = await _connection.QueryAsync<Message>($"SELECT Author, Post, Timestamp FROM Messages WHERE Author = '{user}'");
+            var ownMessages = await _connection.QueryAsync<Message>($"SELECT Author, Post, Timestamp " +
+                                                                    $"FROM Messages WHERE Author = '{user}'");
 
             return messagesSubscription.Concat(ownMessages).OrderByDescending(message => message.Timestamp);
         }
